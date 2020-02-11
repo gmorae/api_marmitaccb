084ea184e2fc2c34ba50324077f5b6747a5000af
gmorae <gmmartins06@gmail.com>
2020-02-11 01:21:47 -0200

    up api completa

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
+const cors = require('cors')
+app.use(bodyParser())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATH, PUT, DELETE');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 
 function conexao() {
@@ -18,12 +29,38 @@ app.listen(process.env.PORT || 8080, () => {
     console.log('Servidor funcionando !')
 })
 
-app.get("/", (req, res) => {
+app.get("/users", (req, res) => {
     const sql = "select * from users"
     conexao().query(sql, (erro, ln, cl) => {
-        console.log("Listagem")
         res.json(ln)
-
     })
 })
 
+app.post('/users', (req, res) => {
+    var name_user = req.body.name_user
+    var qdt_marmita = req.body.qdt_marmita
+    var entrega = req.body.entrega
+    var endereco = req.body.endereco
+    const sql = "INSERT INTO users (name_user, qdt_marmita, entrega, endereco) VALUES ( ?, ?, ?, ?);"
+    conexao().query(sql, [name_user, qdt_marmita, entrega, endereco], (erro, result, fields) => {
+        if (erro) {
+            res.sendStatus(500)
+            return
+        }
+    })
+    res.end()
+})
+
+app.get("/entrega", (req, res) => {
+    const sql = "select * from users where entrega = 'Entrega residêncial'"
+    conexao().query(sql, (erro, ln, cl) => {
+        res.json(ln)
+    })
+})
+
+app.get("/retirada", (req, res) => {
+    const sql = "select * from users where entrega = 'Retirada igreja'"
+    conexao().query(sql, (erro, ln, cl) => {
+        res.json(ln)
+    })
+})
